3b2a86c9a779e2a5e159a1de8851eee3f5d16b32
AllusInteractive <AllusInteractive@users.noreply.github.com>
2022-06-02 11:11:57 +0100

    Created OneCardManager script

---
+++ Assets/Scripts/Visual/OneCardManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// holds the refs to all the Text, Images on the card
+public class OneCardManager : MonoBehaviour {
+
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text NameText;
+    public Text ManaCostText;
+    public Text DescriptionText;
+    public Text HealthText;
+    public Text AttackText;
+    [Header ("GameObject References")]
+    public GameObject HealthIcon;
+    public GameObject AttackIcon;
+    [Header("Image References")]
+    public Image CardGraphicImage;
+    public Image CardBodyImage;
+    public Image CardFaceFrameImage;
+    public Image CardFaceGlowImage;
+    public Image CardBackGlowImage;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCardFromAsset();
+    }
+
+    private bool canBePlayedNow = false;
+    public bool CanBePlayedNow
+    {
+        get
+        {
+            return canBePlayedNow;
+        }
+
+        set
+        {
+            canBePlayedNow = value;
+
+            CardFaceGlowImage.enabled = value;
+        }
+    }
+
+    public void ReadCardFromAsset()
+    {
+        NameText.text = cardAsset.name;
+        ManaCostText.text = cardAsset.ManaCost.ToString();
+        DescriptionText.text = cardAsset.Description;
+        CardGraphicImage.sprite = cardAsset.CardImage;
+
+        if (cardAsset.MaxHealth != 0)
+        {
+            // this is a creature
+            AttackText.text = cardAsset.Attack.ToString();
+            HealthText.text = cardAsset.MaxHealth.ToString();
+        }
+
+        if (PreviewManager != null)
+        {
+            // gae object is a card and not a preview
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+    }
+}
